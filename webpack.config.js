7ded2e698e127e25d776eaa5ed8beec721f26cbb
Christophe HAMERLING <chamerling@linagora.com>
2020-07-15 16:09:05 +0200

    openpaas-suite/esn#3 Bootstrap module configuration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: "source-map",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externals: {
+    angular: 'angular',
+    lodash: 'lodash'
+  }
+};
